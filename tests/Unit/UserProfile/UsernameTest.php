fc8972a32ef1ba913effcc108a185f951fbe3490
Golluméo <p.mauriello54@gmail.com>
2025-10-03 17:16:54 +0200

    test(UserProfile): add unit tests for `Username` validation

---
+++ tests/Unit/UserProfile/UsernameTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Unit: Username', function (): void {
+    it('cannot be empty', function (?string $username): void {
+        expect(fn () => new Username($username))->toThrow(InvalidUsername::class);
+    })->with(['', null]);
+
+    it('cannot be too long (<= 240 chars)', function (): void {
+        expect(fn () => new Username(str_repeat('a', 241)))->toThrow(TooLongUsername::class);
+    });
+
+    it('can be created with a valid length', function (): void {
+        expect(fn () => new Username(str_repeat('a', 15)))->not->toThrow(Throwable::class);
+    });
+});
