3ab10f5a6596d1fb446f40aae4200da6b03d5609
Golluméo <p.mauriello54@gmail.com>
2025-10-04 12:34:00 +0200

    test(UserProfile): update `Username` test to distinguish invalid and null values

--- tests/Unit/UserProfile/UsernameTest.php
+++ tests/Unit/UserProfile/UsernameTest.php
@@ -2,10 +2,20 @@
 
 declare(strict_types=1);
 
+use Domain\User\Exceptions\InvalidUsername;
+use Domain\User\ValueObjects\Username;
+
 describe('Unit: Username', function (): void {
-    it('cannot be empty', function (?string $username): void {
-        expect(fn () => new Username($username))->toThrow(InvalidUsername::class);
-    })->with(['', null]);
+    it('cannot be empty', function (): void {
+        expect(
+            fn () => new Username('')
+        )->toThrow(InvalidUsername::class)
+            ->and(
+                // @phpstan-ignore-next-line
+                fn () => new Username(null)
+            )->toThrow(TypeError::class);
+
+    });
 
     it('cannot exceed 240 chars', function (): void {
         expect(fn () => new Username(str_repeat('a', 241)))->toThrow(TooLongUsername::class);
