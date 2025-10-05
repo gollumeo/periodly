38ef769ab7303c447dc136feca9764f6fc123bb5
Golluméo <p.mauriello54@gmail.com>
2025-10-05 16:27:07 +0200

    feat(UserProfile): enforce charset validation in `UserPassword` with `InvalidCharsetUserPassword` exception

--- src/Domain/User/ValueObjects/UserPassword.php
+++ src/Domain/User/ValueObjects/UserPassword.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Domain\User\ValueObjects;
 
+use Domain\User\Exceptions\InvalidCharsetUserPassword;
 use Domain\User\Exceptions\InvalidUserPassword;
 use Domain\User\Exceptions\TooShortUserPassword;
 
@@ -21,6 +22,10 @@ final readonly class UserPassword
 
         if (mb_strlen($this->password) < 6) {
             throw new TooShortUserPassword();
+        }
+
+        if (! preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/', $this->password)) {
+            throw new InvalidCharsetUserPassword();
         }
     }
 }
