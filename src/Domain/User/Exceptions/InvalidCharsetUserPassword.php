2d5e186c1dbf1ef4568f98608ea7d8e86431705f
Golluméo <p.mauriello54@gmail.com>
2025-10-05 16:27:20 +0200

    feat(User): add `InvalidCharsetUserPassword` exception for password charset enforcement

---
+++ src/Domain/User/Exceptions/InvalidCharsetUserPassword.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\User\Exceptions;
+
+use Exception;
+
+final class InvalidCharsetUserPassword extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct('Password must contain at least one uppercase letter, one lowercase letter, one number and a special character.');
+    }
+}
