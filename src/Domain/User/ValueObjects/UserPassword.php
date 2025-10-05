237ceb3d816ebde5ddba936d3af3eb94a1cbddf8
Golluméo <p.mauriello54@gmail.com>
2025-10-05 11:54:19 +0200

    feat(UserProfile): enforce minimum length validation in `UserPassword` with `TooShortUserPassword` exception

--- src/Domain/User/ValueObjects/UserPassword.php
+++ src/Domain/User/ValueObjects/UserPassword.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace Domain\User\ValueObjects;
 
 use Domain\User\Exceptions\InvalidUserPassword;
+use Domain\User\Exceptions\TooShortUserPassword;
 
 final readonly class UserPassword
 {
     /**
      * @throws InvalidUserPassword
+     * @throws TooShortUserPassword
      */
     public function __construct(private string $password)
     {
         if ($this->password === '') {
             throw new InvalidUserPassword();
+        }
+
+        if (mb_strlen($this->password) < 6) {
+            throw new TooShortUserPassword();
         }
     }
 }
