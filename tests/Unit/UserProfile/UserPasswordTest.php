1dfcfc786d7e8f233a0fe1e8fc35cfdc758bf804
Golluméo <p.mauriello54@gmail.com>
2025-10-05 11:54:12 +0200

    test(UserProfile): add unit test for `UserPassword` to enforce minimum length validation

--- tests/Unit/UserProfile/UserPasswordTest.php
+++ tests/Unit/UserProfile/UserPasswordTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Domain\User\Exceptions\InvalidUserPassword;
+use Domain\User\Exceptions\TooShortUserPassword;
 use Domain\User\ValueObjects\UserPassword;
 
 describe('Unit: Password', function (): void {
@@ -14,5 +15,11 @@ describe('Unit: Password', function (): void {
                 // @phpstan-ignore-next-line
                 fn () => new UserPassword(null)
             )->toThrow(TypeError::class);
+    });
+
+    it('must be at least 6 characters', function (): void {
+        expect(
+            fn () => new UserPassword('123')
+        )->toThrow(TooShortUserPassword::class);
     });
 });
