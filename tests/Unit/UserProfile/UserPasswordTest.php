c9f0d66423b3c04b2af4c2471a1b80bc1343dfd7
Golluméo <p.mauriello54@gmail.com>
2025-10-05 16:27:03 +0200

    test(UserProfile): extend `UserPassword` tests for charset validation and valid password cases

--- tests/Unit/UserProfile/UserPasswordTest.php
+++ tests/Unit/UserProfile/UserPasswordTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Domain\User\Exceptions\InvalidCharsetUserPassword;
 use Domain\User\Exceptions\InvalidUserPassword;
 use Domain\User\Exceptions\TooShortUserPassword;
 use Domain\User\ValueObjects\UserPassword;
@@ -21,5 +22,27 @@ describe('Unit: Password', function (): void {
         expect(
             fn () => new UserPassword('123')
         )->toThrow(TooShortUserPassword::class);
+    });
+
+    it('must respect a specific charset', function (string $password): void {
+        expect(
+            /**
+             * @throws TooShortUserPassword
+             * @throws InvalidUserPassword
+             */
+            fn () => new UserPassword($password)
+        )->toThrow(InvalidCharsetUserPassword::class);
+    })->with([
+        'password123.',
+        'PASSWORD123.',
+        'Password.',
+        'Password123',
+        'password',
+    ]);
+
+    it('accepts a valid password', function (): void {
+        expect(
+            fn () => new UserPassword('MySuperPassword1!')
+        )->not->toThrow(Throwable::class);
     });
 });
