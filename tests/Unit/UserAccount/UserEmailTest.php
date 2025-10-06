412eb4ea35a47e8ba14167898785b0c29ba20144
Golluméo <p.mauriello54@gmail.com>
2025-10-06 15:49:59 +0200

    test(UserAccount): extend `UserEmail` tests to include null value validation

--- tests/Unit/UserAccount/UserEmailTest.php
+++ tests/Unit/UserAccount/UserEmailTest.php
@@ -2,10 +2,17 @@
 
 declare(strict_types=1);
 
+use Domain\User\Exceptions\UserAccount\UserEmail\InvalidUserEmail;
+use Domain\User\ValueObjects\UserAccount\UserEmail;
+
 describe('Unit: User Email', function (): void {
     it('cannot be empty', function (): void {
         expect(
             fn () => new UserEmail('')
-        )->toThrow(InvalidUserEmail::class);
+        )->toThrow(InvalidUserEmail::class)
+            ->and(
+                // @phpstan-ignore-next-line
+                fn () => new UserEmail(null)
+            )->toThrow(TypeError::class);
     });
 });
