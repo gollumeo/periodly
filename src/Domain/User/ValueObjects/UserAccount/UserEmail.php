bf331a83fcc54ee220adc2cdc3d423d0b046b8bf
Golluméo <p.mauriello54@gmail.com>
2025-10-08 15:36:38 +0200

    feat(UserAccount): add `InvalidUserEmailCharset` exception for email charset validation

--- src/Domain/User/ValueObjects/UserAccount/UserEmail.php
+++ src/Domain/User/ValueObjects/UserAccount/UserEmail.php
@@ -5,16 +5,21 @@ declare(strict_types=1);
 namespace Domain\User\ValueObjects\UserAccount;
 
 use Domain\User\Exceptions\UserAccount\UserEmail\InvalidUserEmail;
+use Domain\User\Exceptions\UserAccount\UserEmail\InvalidUserEmailCharset;
 
 final class UserEmail
 {
     /**
-     * @throws InvalidUserEmail
+     * @throws InvalidUserEmail|InvalidUserEmailCharset
      */
     public function __construct(string $userEmail)
     {
         if ($userEmail === '') {
             throw new InvalidUserEmail();
+        }
+
+        if (! preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i', $userEmail)) {
+            throw new InvalidUserEmailCharset();
         }
     }
 }
