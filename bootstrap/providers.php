03c59e1cfdd22fdcb58afbc264ed920adc4e9b4c
Golluméo <p.mauriello54@gmail.com>
2025-10-10 13:13:39 +0200

    feat(Laravel): register `ExceptionServiceProvider` in providers list

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use Infrastructure\Laravel\Providers\Core\ExceptionServiceProvider;
 use Infrastructure\Laravel\Providers\Core\MiddlewareServiceProvider;
 use Infrastructure\Laravel\Providers\Core\RouteServiceProvider;
 
 return [
     RouteServiceProvider::class,
     MiddlewareServiceProvider::class,
+    ExceptionServiceProvider::class,
 ];
