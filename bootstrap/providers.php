250abaf1da488c187f58e64f56355755600cb1b8
Golluméo <p.mauriello54@gmail.com>
2025-10-10 12:53:46 +0200

    feat(Laravel): register `RouteServiceProvider` and `MiddlewareServiceProvider` in providers list

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,10 @@
 
 declare(strict_types=1);
 
+use Infrastructure\Laravel\Providers\Core\MiddlewareServiceProvider;
+use Infrastructure\Laravel\Providers\Core\RouteServiceProvider;
+
 return [
+    RouteServiceProvider::class,
+    MiddlewareServiceProvider::class,
 ];
