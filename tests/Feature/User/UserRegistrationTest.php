44da7638126df00794f96c452af8bb4aa0e2ef81
Golluméo <p.mauriello54@gmail.com>
2025-10-12 14:38:38 +0200

    test(UserRegistration): add placeholder test for user registration feature

---
+++ tests/Feature/User/UserRegistrationTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Feature: User registration', function (): void {
+    it('creates a new user based on user account data', function (): void {
+        //
+    });
+})->skip('todo');
