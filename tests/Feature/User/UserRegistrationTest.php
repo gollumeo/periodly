a2649e11cf0a582892007e7b12ef0ab2159c9015
Golluméo <p.mauriello54@gmail.com>
2025-10-13 12:15:14 +0200

    test(UserRegistration): implement base test setup with user account creation objects

--- tests/Feature/User/UserRegistrationTest.php
+++ tests/Feature/User/UserRegistrationTest.php
@@ -2,8 +2,17 @@
 
 declare(strict_types=1);
 
+use Domain\User\ValueObjects\UserAccount\UserEmail;
+use Domain\User\ValueObjects\UserAccount\Username;
+
 describe('Feature: User registration', function (): void {
     it('creates a new user based on user account data', function (): void {
-        //
+        $email = new UserEmail('test@test.com');
+        $username = new Username('d@mn-periods');
+        $password = new Password('Strongp4ss!');
+        $userAccount = new UserAccount($email, $username, $password);
+
+        $registerUser = new RegisterUser(); // use case
+        $userId = ($registerUser)($userAccount); // invokable
     });
 })->skip('todo');
