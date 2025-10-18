661b812d85c806378513b8eb38a8d221b2dfdfa2
Golluméo <p.mauriello54@gmail.com>
2025-10-18 14:03:31 +0200

    test(UserRegistration): fix variable name typo in user registration test

--- tests/Feature/Application/User/UserRegistrationTest.php
+++ tests/Feature/Application/User/UserRegistrationTest.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 describe('Feature: User registration', function (): void {
     it('registers a new user with valid credentials', function (): void {
         $users = new InMemoryUsers();
-        $passwordHash = new HashPassword();
+        $hashPassword = new HashPassword();
         $events = new FakeEventBus();
 
-        $registerUser = new RegisterUser($users, $passwordHash, $events);
+        $registerUser = new RegisterUser($users, $hashPassword, $events);
 
         $userAccount = new UserAccount(
             new Username('d@mn-periods'),
