00c9b2d5b4f40e340e6e33f839d3b965c32c2b06
Golluméo <p.mauriello54@gmail.com>
2025-11-12 18:42:49 +0100

    test(Username): update tests to use `Username::fromString` for object creation consistency

--- tests/Unit/User/UserAccount/UsernameTest.php
+++ tests/Unit/User/UserAccount/UsernameTest.php
@@ -9,24 +9,24 @@ use Domain\User\ValueObjects\UserAccount\Username;
 describe('Unit: Username', function (): void {
     it('cannot be empty', function (): void {
         expect(
-            fn () => new Username('')
+            fn () => Username::fromString('')
         )->toThrow(InvalidUsername::class)
             ->and(
                 // @phpstan-ignore-next-line
-                fn () => new Username(null)
+                fn () => Username::fromString(null)
             )->toThrow(TypeError::class);
 
     });
 
     it('cannot exceed 240 chars', function (): void {
         expect(
-            fn () => new Username(str_repeat('a', 241))
+            fn () => Username::fromString(str_repeat('a', 241))
         )->toThrow(TooLongUsername::class);
     });
 
     it('accepts a valid length', function (): void {
         expect(
-            fn () => new Username(str_repeat('a', 15))
+            fn () => Username::fromString(str_repeat('a', 15))
         )->not->toThrow(Throwable::class);
     });
 });
