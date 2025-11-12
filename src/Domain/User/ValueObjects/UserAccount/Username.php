c8622cf2270012d6d9b1dfa8f3b5317f09b3f3d7
Golluméo <p.mauriello54@gmail.com>
2025-11-12 18:42:35 +0100

    feat(ValueObjects): refactor `Username` to immutable and enhance creation logic

--- src/Domain/User/ValueObjects/UserAccount/Username.php
+++ src/Domain/User/ValueObjects/UserAccount/Username.php
@@ -9,17 +9,30 @@ use Domain\User\Exceptions\UserAccount\Username\TooLongUsername;
 
 final readonly class Username
 {
-    /**
-     * @throws InvalidUsername|TooLongUsername
-     */
-    public function __construct(private string $username)
+    public const int MAX_CHARS = 240;
+
+    public function __construct(private string $value) {}
+
+    public function __toString(): string
     {
-        if ($this->username === '') {
+        return $this->value;
+    }
+
+    public static function fromString(string $value): self
+    {
+        if ($value === '') {
             throw new InvalidUsername();
         }
 
-        if (mb_strlen($this->username) > 240) {
+        if (mb_strlen($value) > self::MAX_CHARS) {
             throw new TooLongUsername();
         }
+
+        return new self($value);
+    }
+
+    public function value(): string
+    {
+        return $this->value;
     }
 }
