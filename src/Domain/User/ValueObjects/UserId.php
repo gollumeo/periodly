b2aa4e7e5a3481c2c4bc297b268a5a45fe6fa0af
Golluméo <p.mauriello54@gmail.com>
2025-11-15 12:56:15 +0100

    refactor(UserId): update `UuidContract` namespace and simplify constructor formatting

--- src/Domain/User/ValueObjects/UserId.php
+++ src/Domain/User/ValueObjects/UserId.php
@@ -1,18 +1,16 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Domain\User\ValueObjects;
 
-use Application\Contracts\User\UuidContract;
+use Application\Contracts\Shared\UuidContract;
 
 final readonly class UserId
 {
     private function __construct(
         private string $value,
-    )
-    {
-    }
+    ) {}
 
     public static function generate(UuidContract $uuid): self
     {
