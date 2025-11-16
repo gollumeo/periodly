5a70090f2705602a956839c5a746df60663aec59
Golluméo <p.mauriello54@gmail.com>
2025-11-16 21:44:40 +0100

    test(UserRegistration): remove outdated test and mark with `todo` for further review

--- tests/Feature/Application/User/UserRegistrationTest.php
+++ tests/Feature/Application/User/UserRegistrationTest.php
@@ -2,28 +2,6 @@
 
 declare(strict_types=1);
 
-use Domain\User\ValueObjects\UserAccount\PlainPassword;
-use Domain\User\ValueObjects\UserAccount\UserAccount;
-use Domain\User\ValueObjects\UserAccount\UserEmail;
-use Domain\User\ValueObjects\UserAccount\Username;
-use Domain\User\ValueObjects\UserId;
-
 describe('Feature: Register User', function (): void {
-    it('registers a new user with valid credentials', function (): void {
-        $users = new InMemoryUsers();
-        $hashPassword = new FakeHashPassword();
-        $events = new FakeEventBus();
-
-        $registerUser = new RegisterUser($users, $hashPassword, $events);
-
-        $userAccount = new UserAccount(
-            new UserEmail('test@test.com'),
-            new Username('Gollum'),
-            new PlainPassword('Strongp4ssword!')
-        );
-
-        $userId = $registerUser($userAccount);
-
-        expect($userId)->toBeInstanceOf(UserId::class);
-    });
+    todo('later');
 });
