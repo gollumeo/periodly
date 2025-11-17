3267abae0fae7c2b6e6dc174428e0a3feb2032f3
Golluméo <p.mauriello54@gmail.com>
2025-11-17 18:27:29 +0100

    test(EmailAddress): add test to validate handling of empty or whitespace-only email strings

---
+++ tests/Domain/Account/ValueObjects/EmailAddressTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Domain: Account Email Address', function (): void {
+    it('cannot be empty or only whitespace', function (string $rawEmail): void {
+        expect(fn () => EmailAddress::fromString($rawEmail))
+            ->toThrow(InvalidEmailAddress::class);
+    })->with([
+        '',
+        '    ',
+        "   \t   ",
+        "\n",
+        "\r\n",
+    ]);
+});
